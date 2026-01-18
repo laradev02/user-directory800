a4b1987859f2b76d241e3b1a78f136c9d75e8f08
Lara Mohti <laramohti@gmail.com>
2026-01-18 21:56:51 +0200

    Refactor loading bar component to use Angular Material progress bar and combine loading states from HTTP and user store. Updated styles and removed deprecated loading animations.

--- src/app/components/loading-bar/loading-bar.component.ts
+++ src/app/components/loading-bar/loading-bar.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { LoadingService } from '../../core/services/loading.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import * as fromUsers from '../../store/users/users.selectors';
 
 @Component({
   selector: 'app-loading-bar',
@@ -9,53 +13,63 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   imports: [CommonModule, MatProgressBarModule],
   template: `
     <div class="loading-bar-container" *ngIf="loading$ | async">
-      <div class="loading-bar">
-        <div class="loading-shimmer"></div>
-      </div>
+      <mat-progress-bar
+        mode="indeterminate"
+        color="primary"
+        class="loading-progress-bar">
+      </mat-progress-bar>
     </div>
   `,
   styles: [`
     .loading-bar-container {
       position: sticky;
-      top: 80px;
+      top: 75px;
       left: 0;
       right: 0;
       z-index: 999;
-      height: 3px;
-      background: #f0fdf4;
-      margin-bottom: 0;
+      height: 4px;
+      margin: 0;
+      padding: 0;
     }
 
-    .loading-bar {
-      height: 100%;
-      width: 65%;
-      position: relative;
-      overflow: hidden;
-    }
-
-    .loading-shimmer {
+    .loading-progress-bar {
       height: 100%;
       width: 100%;
-      background: linear-gradient(90deg, #10b981 0%, #34d399 50%, #10b981 100%);
-      background-size: 200% 100%;
-      animation: shimmer 2s infinite linear;
-      border-radius: 0 9999px 9999px 0;
-    }
 
-    @keyframes shimmer {
-      0% {
-        background-position: -200% 0;
-      }
-      100% {
-        background-position: 200% 0;
+      ::ng-deep {
+        .mdc-linear-progress__buffer {
+          background-color: rgba(16, 185, 129, 0.1) !important;
+        }
+
+        .mdc-linear-progress__bar-inner {
+          background-color: #10b981 !important;
+          border-color: #10b981 !important;
+        }
+
+        .mat-mdc-progress-bar .mdc-linear-progress__buffer {
+          background-color: rgba(16, 185, 129, 0.1) !important;
+        }
+
+        .mat-mdc-progress-bar .mdc-linear-progress__bar-inner {
+          background-color: #10b981 !important;
+        }
       }
     }
   `]
 })
-export class LoadingBarComponent implements OnInit {
-  loading$ = this.loadingService.loading$;
+export class LoadingBarComponent {
+  loading$: Observable<boolean>;
 
-  constructor(private loadingService: LoadingService) {}
-
-  ngOnInit(): void {}
+  constructor(
+    private loadingService: LoadingService,
+    private store: Store
+  ) {
+    // Combine HTTP loading and users store loading
+    const httpLoading$ = this.loadingService.loading$;
+    const usersLoading$ = this.store.select(fromUsers.selectUsersLoading);
+    
+    this.loading$ = combineLatest([httpLoading$, usersLoading$]).pipe(
+      map(([httpLoading, usersLoading]) => httpLoading || usersLoading)
+    );
+  }
 }
